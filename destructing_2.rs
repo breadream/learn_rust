1667393fb91b105aee7e71cd631625ee3e160100
Brad Lim <brad.dh.lim@gmail.com>
2024-01-17 23:02:24 -0800

    Add destructing

---
+++ destructing_2.rs
@@ -0,0 +1,34 @@
+struct City {
+    name: String,
+    name_before: String,
+    population: u32,
+    date_founded: u32,
+}
+
+impl City {
+    fn new(name: String, name_before: String, population: u32, date_founded: u32) -> Self {
+        Self {
+            name,
+            name_before,
+            population,
+            date_founded,
+        }
+    }
+}
+
+fn process_city_values(city: &City) {
+    let City {
+        name,
+        name_before,
+        population,
+        date_founded,
+    } = city;
+    // now we have the values to use separately
+    let two_names = vec![name, name_before];
+    println!("The city's two names are {:?}", two_names);
+}
+
+fn main() {
+    let tallinn = City::new("Tallinn".to_string(), "Reval".to_string(), 427_539, 1212);
+    process_city_values(&tallinn);
+}

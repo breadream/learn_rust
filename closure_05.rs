d8c5e8f5005c8cfff900ca78b8358ee495eeb669
Brad Lim <brad.dh.lim@gmail.com>
2024-02-01 21:30:08 -0800

    Add ok_or_else option

---
+++ closure_05.rs
@@ -0,0 +1,42 @@
+struct Company {
+    name: String,
+    ceo: Option<String>,
+}
+
+impl Company {
+    fn new(name: &str, ceo: &str) -> Self {
+        let ceo = match ceo {
+            "" => None,
+            name => Some(name.to_string()),
+        };
+        Self {
+            name: name.to_string(),
+            ceo,
+        }
+    }
+
+    fn get_ceo(&self) -> Option<String> {
+        self.ceo.clone()
+    }
+}
+
+fn main() {
+    let company_vec = vec![
+        Company::new("Tesla", "Elon"),
+        Company::new("Mazda", "Mazda sang"),
+        Company::new("MALTS", ""),
+    ];
+
+    let mut results_vec = vec![];
+
+    company_vec.iter().for_each(|company| {
+        results_vec.push(company.get_ceo().ok_or_else(|| {
+            let err_msg = format!("No CEO found for {}", company.name);
+            err_msg
+        }))
+    });
+
+    for item in results_vec {
+        println!("{:?}", item);
+    }
+}

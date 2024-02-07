bd9c4c9215aace3b930e63fd63eb6874940049ed
Brad Lim <brad.dh.lim@gmail.com>
2024-02-06 22:36:06 -0800

    Add Rc example

---
+++ Rs.rs
@@ -0,0 +1,31 @@
+use std::rc::Rc; // Rc clones the pointer
+
+#[derive(Debug)]
+struct City {
+    name: String,
+    population: u32,
+    city_history: Rc<String>,
+}
+
+#[derive(Debug)]
+struct CityData {
+    names: Vec<String>,
+    histories: Vec<Rc<String>>,
+}
+
+fn main() {
+    let calgary = City {
+        name: "Calgary".to_string(),
+        population: 1_200_000,
+        city_history: Rc::new("Very long string............".to_string()),
+    };
+
+    let canada_cities = CityData {
+        names: vec![calgary.name],
+        histories: vec![calgary.city_history.clone()],
+    };
+
+    println!("Calgary's history is :{}", calgary.city_history);
+    println!("{}", Rc::strong_count(&calgary.city_history));
+    let new_owner = calgary.city_history.clone();
+}

da597ab58a12dc43f4918c7f794ad50904ec5aaa
Brad Lim <brad.dh.lim@gmail.com>
2024-02-12 22:42:12 -0800

    Add builder pattern example

--- default_1.rs
+++ default_1.rs
@@ -5,6 +5,7 @@ struct Character {
     height: u32,
     weight: u32,
     lifestate: LifeState,
+    can_use: bool,
 }
 
 #[derive(Debug)]
@@ -16,38 +17,68 @@ enum LifeState {
 }
 
 impl Character {
-    fn new(name: String, age: u8, height: u32, weight: u32, alive: bool) -> Self {
-        Self {
-            name,
-            age,
-            height,
-            weight,
-            lifestate: if alive {
-                LifeState::Alive
-            } else {
-                LifeState::Dead
-            },
-        }
-    }
-}
-
-impl Default for Character {
-    fn default() -> Self {
+    fn new() -> Self {
         Self {
             name: "Bill".to_string(),
-            age: 30,
-            height: 200,
-            weight: 10,
+            age: 15,
+            height: 170,
+            weight: 70,
             lifestate: LifeState::Alive,
+            can_use: true,
+        }
+    }
+
+    fn height(mut self, height: u32) -> Self {
+        self.height = height;
+        self.can_use = false;
+        self
+    }
+
+    fn weight(mut self, weight: u32) -> Self {
+        self.weight = weight;
+        self.can_use = false;
+        self
+    }
+
+    fn name(mut self, name: &str) -> Self {
+        self.name = name.to_string();
+        self.can_use = false;
+        self
+    }
+
+    fn build(mut self) -> Result<Character, String> {
+        if self.height < 200 && self.weight < 300 && !self.name.to_lowercase().contains("smurf") {
+            self.can_use = true;
+            Ok(self)
+        } else {
+            Err("Could not create character. Characters must have:
+                1) Height below 200
+                2) Weight below 300
+                3) A name that is not Smurf (that is a bad word)"
+                    .to_string())
         }
     }
 }
 
-fn main() {
-    let character_1 = Character::default();
-    println!(
-        "The character {:?} is {:?} years old",
-        character_1.name, character_1.age
-    );
-}
 
+fn main() {
+    let character_with_smurf = Character::new().name("Lol I am Smurf!!").build(); // This one contains "smurf" - not okay
+    let character_too_tall = Character::new().height(400).build(); // Too tall - not okay
+    let character_too_heavy = Character::new().weight(500).build(); // Too heavy - not okay
+    let okay_character = Character::new()
+        .name("Billybrobby")
+        .height(180)
+        .weight(100)
+        .build();   // This character is okay. Name is fine, height and weight are fine
+
+    // Now they are not Character, they are Result<Character, String>. So let's put them in a Vec so we can see them:
+    let character_vec = vec![character_with_smurf, character_too_tall, character_too_heavy, okay_character];
+
+    for character in character_vec { // Now we will print the character if it's Ok, and print the error if it's Err
+        match character {
+            Ok(character_info) => println!("{:?}", character_info),
+            Err(err_info) => println!("{}", err_info),
+        }
+        println!(); // Then add one more line
+    }
+}
